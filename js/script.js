47d8d945a6047389e23bd083602d355eaa43e6c3
zzirco <zzirco@naver.com>
2025-08-01 16:53:00 +0900

    [feat] 프로그래밍 기초 3. JS 1일차

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.write("<h1>Hello SKALA</h1>");
+document.write("<h2>Hello SKALA</h2>");
+document.write("<h3>Hello SKALA</h3>");
+document.write("<h4>Hello SKALA</h4>");
+document.write("<h5>Hello SKALA</h5>");
+document.write("<h6>Hello SKALA</h6>");
